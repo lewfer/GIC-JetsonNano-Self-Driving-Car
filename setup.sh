f4da123d062774d5f6981a51042ca17fa2fe82bb
lewfer <llewelyn@arcwave.co.uk>
2022-08-12 17:42:17 +0100

    Added notebooks and setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# Setup code overrides for GIC activity
+
+cp usr/bin/pwm.sh /usr/bin/
+sudo chmod +x /usr/bin/pwm.sh
+
+cp etc/systemd/system/jetbot_pwm.service /etc/systemd/system/
+
+sudo systemctl start jetbot_pwm.service
+sudo systemctl enable jetbot_pwm.service
+
+mv ~/jetbot/jetbot/robot.py robot_original.py
+cp jetbot/jetbot/robot.py ~/jetbot/jetbot/
+cp jetbot/jetbot/robot.py /usr/local/lib/python3.6/dist-packages/jetbot-0.4.3-py3.6.egg/jetbot
+
+mkdir ~/jetbot/notebooks/gic
+cp jetbot/notebooks/gic ~/jetbot/notebooks/gic
