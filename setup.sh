d39a49a90bea4d7787db0ef1109792df605933e9
lewfer <llewelyn@arcwave.co.uk>
2022-08-17 08:35:25 +0100

    Notebook tweaks and improvements

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 # Setup code overrides for GIC activity
 
+# Script and service to set pins 32 and 33 to PWM
 sudo cp usr/bin/pwm.sh /usr/bin/
 sudo chmod +x /usr/bin/pwm.sh
-
 sudo cp etc/systemd/system/jetbot_pwm.service /etc/systemd/system/
-
 sudo systemctl start jetbot_pwm.service
 sudo systemctl enable jetbot_pwm.service
 
+# Code to use L298N motor driver instead of the Adafruit one
 mv ~/jetbot/jetbot/robot.py robot_original.py
 cp jetbot/jetbot/robot.py ~/jetbot/jetbot/
 cp jetbot/jetbot/robot.py /usr/local/lib/python3.6/dist-packages/jetbot-0.4.3-py3.6.egg/jetbot
 
+# GIC workshop code
 mkdir ~/jetbot/notebooks/gic
-cp jetbot/notebooks/gic ~/jetbot/notebooks/gic
+cp -R jetbot/notebooks/gic ~/jetbot/notebooks/gic
